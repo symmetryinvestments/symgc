632653e683c5e8f8cda9a0a9a7d85143b677312b
Steven Schveighoffer <schveiguy@gmail.com>
2025-03-26 23:01:10 -0400

    Add verbosity flag to test harness, and summary output.

--- test/source/app.d
+++ test/source/app.d
@@ -41,7 +41,7 @@ TestHarness parseTest(string filename)
 	return harness;
 }
 
-void runTest(ref TestHarness th, bool verbose)
+bool runTest(ref TestHarness th, bool verbose)
 {
 	import std.process;
 	auto result = execute(["dub", "--single", th.filename]);
@@ -50,6 +50,7 @@ void runTest(ref TestHarness th, bool verbose)
 		writeln(i"TEST $(th.filename) FAILED with return code $(th.retval), expected $(th.expectedRetval)");
 		writeln("test output:");
 		writeln(result.output);
+		return false;
 	}
 	else
 	{
@@ -59,19 +60,32 @@ void runTest(ref TestHarness th, bool verbose)
 			writeln("test output:");
 			writeln(result.output);
 		}
+		return true;
 	}
 }
 
-void main()
+int main(string[] args)
 {
+	import std.getopt;
+	bool verbose = false;
+	auto helpInformation = getopt(args, "v|verbose", &verbose);
+	if(helpInformation.helpWanted)
+	{
+		defaultGetoptPrinter("Test harness for Symgc integration tests.",
+				helpInformation.options);
+		return 1;
+	}
 	TestHarness[] tests = dirEntries("valid", SpanMode.shallow)
 		.filter!(de => de.isFile && de.name.endsWith(".d"))
 		.map!(de => parseTest(de.name))
 		.array;
 	tests.sort!((ref TestHarness a, ref TestHarness b) => a.filename < b.filename);
 
+	int npassed = 0;
 	foreach(ref th; tests)
 	{
-		th.runTest(verbose: true);
+		npassed += th.runTest(verbose: verbose);
 	}
+	writeln(i"SUMMARY: $(tests.length) Tests run, $(npassed) PASSED, $(tests.length - npassed) FAILED");
+	return npassed == tests.length ? 0 : 1;
 }
