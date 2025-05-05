93051d12a983789703df1e4c6eefccc7b208cf1a
Steven Schveighoffer <schveiguy@gmail.com>
2025-05-05 16:08:08 -0400

    Make sure we don't break Linux support.

--- source/symgc/thread.d
+++ source/symgc/thread.d
@@ -3,11 +3,13 @@ module symgc.thread;
 version(Windows) {
 	import core.sys.windows.winnt : HANDLE;
 	import core.sys.windows.windef : DWORD;
+	public import core.sys.windows.winbase : currentThreadHandle = GetCurrentThread;
 	alias ThreadHandle = HANDLE;
 	private enum THREAD_RETURN_VALUE = DWORD(0);
 } else version(linux) {
-	import core.sys.posix.pthread : pthread_t;
+	import core.sys.posix.pthread : pthread_t, pthread_self;
 	alias ThreadHandle = pthread_t;
+	alias currentThreadHandle = pthread_self;
 	private enum THREAD_RETURN_VALUE = null;
 }
 
@@ -15,15 +17,11 @@ version(linux):
 
 import d.gc.thread;
 
-// thread start function application code will use.
-// The return value is always void, because we don't use it in this library.
-alias ThreadStartRoutine = void function(void*);
-
 /**
  * Create a GC thread that does not prevent stop the world (or block waiting for world stopping to finish)
  */
-bool createGCThread(ThreadHandle* thread, ThreadStartRoutine start_routine, void* arg) {
-	auto runner = ThreadRunner.alloc(start_routine, arg);
+bool createGCThread(TSR)(ThreadHandle* thread, TSR start_routine, void* arg) {
+	auto runner = allocThreadRunner(start_routine, arg);
 
 	version(linux) {
 		alias OSThreadRoutine = extern(C) void* function(void*);
@@ -36,32 +34,37 @@ bool createGCThread(ThreadHandle* thread, ThreadStartRoutine start_routine, void
 			import core.sys.posix.pthread : start_thread = pthread_create;
 		}
 		auto ret = start_thread(
-			thread, null, cast(OSThreadRoutine) &runThread!false, runner);
+			thread, null, cast(OSThreadRoutine) runner.getFunction!false(), runner);
 		return ret == 0;
 	} else version(Windows) {
 		import core.sys.windows.winbase : LPTHREAD_START_ROUTINE, CreateThread, INVALID_HANDLE_VALUE;
-		*thread = CreateThread(null, 0, cast(LPTHREAD_START_ROUTINE)&runThread!false, runner, 0, null);
+		*thread = CreateThread(null, 0, cast(LPTHREAD_START_ROUTINE) runner.getFunction!false(), runner, 0, null);
 		return *thread != INVALID_HANDLE_VALUE;
 	}
 }
 
 package:
 
-struct ThreadRunner {
+struct ThreadRunner(TSR) {
 	void* arg;
-	ThreadStartRoutine fun;
+	TSR fun;
 
-	static alloc(ThreadStartRoutine fun, void* arg) {
-		import d.gc.capi : __sd_gc_alloc;
-		// TODO: is this the right way to do this now?
-		auto runner = cast(ThreadRunner*)__sd_gc_alloc(ThreadRunner.sizeof);
-		runner.fun = fun;
-		runner.arg = arg;
-		return runner;
+	static getFunction(bool AllowStopTheWorld)()
+	{
+		return &runThread!(AllowStopTheWorld, typeof(this));
 	}
 }
 
-extern(C) typeof(THREAD_RETURN_VALUE) runThread(bool AllowStopTheWorld)(ThreadRunner* runner) {
+ThreadRunner!TSR* allocThreadRunner(TSR)(TSR fun, void* arg) {
+	alias TRType = ThreadRunner!TSR;
+	import d.gc.capi : __sd_gc_alloc;
+	auto runner = cast(TRType*)__sd_gc_alloc(TRType.sizeof);
+	runner.fun = fun;
+	runner.arg = arg;
+	return runner;
+}
+
+extern(C) auto runThread(bool AllowStopTheWorld, TRunner)(TRunner* runner) {
 	import d.gc.capi : __sd_gc_free;
 	import d.gc.thread : createThread, destroyThread;
 
@@ -74,7 +77,15 @@ extern(C) typeof(THREAD_RETURN_VALUE) runThread(bool AllowStopTheWorld)(ThreadRu
 	// Make sure we clean up after ourselves.
 	scope(exit) destroyThread();
 
-	fun(arg);
-
-	return THREAD_RETURN_VALUE;
+	// If the passed in function returns void, give a valid return to the OS
+	// function.
+	static if(is(typeof(fun(arg)) == void))
+	{
+		fun(arg);
+		return THREAD_RETURN_VALUE;
+	}
+	else
+	{
+		return fun(arg);
+	}
 }
