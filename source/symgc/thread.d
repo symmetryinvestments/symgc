b653c9e684c3a7327cd64aed9a6123c033fd47b6
Steven Schveighoffer <schveiguy@gmail.com>
2025-05-05 16:04:16 -0400

    Spaces to tabs.

--- source/symgc/thread.d
+++ source/symgc/thread.d
@@ -1,14 +1,14 @@
 module symgc.thread;
 
 version(Windows) {
-    import core.sys.windows.winnt : HANDLE;
-    import core.sys.windows.windef : DWORD;
-    alias ThreadHandle = HANDLE;
-    private enum THREAD_RETURN_VALUE = DWORD(0);
+	import core.sys.windows.winnt : HANDLE;
+	import core.sys.windows.windef : DWORD;
+	alias ThreadHandle = HANDLE;
+	private enum THREAD_RETURN_VALUE = DWORD(0);
 } else version(linux) {
-    import core.sys.posix.pthread : pthread_t;
-    alias ThreadHandle = pthread_t;
-    private enum THREAD_RETURN_VALUE = null;
+	import core.sys.posix.pthread : pthread_t;
+	alias ThreadHandle = pthread_t;
+	private enum THREAD_RETURN_VALUE = null;
 }
 
 version(linux):
@@ -25,24 +25,24 @@ alias ThreadStartRoutine = void function(void*);
 bool createGCThread(ThreadHandle* thread, ThreadStartRoutine start_routine, void* arg) {
 	auto runner = ThreadRunner.alloc(start_routine, arg);
 
-    version(linux) {
-        alias OSThreadRoutine = extern(C) void* function(void*);
-        version(Symgc_pthread_hook) {
-            // use the trampoline as the thread create function
-            import symgc.trampoline : start_thread = pthread_create_trampoline;
-        }
-        else {
-            // no trampoline, just use the normal pthread_create
-            import core.sys.posix.pthread : start_thread = pthread_create;
-        }
-        auto ret = start_thread(
-            thread, null, cast(OSThreadRoutine) &runThread!false, runner);
-        return ret == 0;
-    } else version(Windows) {
-        import core.sys.windows.winbase : LPTHREAD_START_ROUTINE, CreateThread, INVALID_HANDLE_VALUE;
-        *thread = CreateThread(null, 0, cast(LPTHREAD_START_ROUTINE)&runThread!false, runner, 0, null);
-        return *thread != INVALID_HANDLE_VALUE;
-    }
+	version(linux) {
+		alias OSThreadRoutine = extern(C) void* function(void*);
+		version(Symgc_pthread_hook) {
+			// use the trampoline as the thread create function
+			import symgc.trampoline : start_thread = pthread_create_trampoline;
+		}
+		else {
+			// no trampoline, just use the normal pthread_create
+			import core.sys.posix.pthread : start_thread = pthread_create;
+		}
+		auto ret = start_thread(
+			thread, null, cast(OSThreadRoutine) &runThread!false, runner);
+		return ret == 0;
+	} else version(Windows) {
+		import core.sys.windows.winbase : LPTHREAD_START_ROUTINE, CreateThread, INVALID_HANDLE_VALUE;
+		*thread = CreateThread(null, 0, cast(LPTHREAD_START_ROUTINE)&runThread!false, runner, 0, null);
+		return *thread != INVALID_HANDLE_VALUE;
+	}
 }
 
 package:
@@ -52,7 +52,7 @@ struct ThreadRunner {
 	ThreadStartRoutine fun;
 
 	static alloc(ThreadStartRoutine fun, void* arg) {
-        import d.gc.capi : __sd_gc_alloc;
+		import d.gc.capi : __sd_gc_alloc;
 		// TODO: is this the right way to do this now?
 		auto runner = cast(ThreadRunner*)__sd_gc_alloc(ThreadRunner.sizeof);
 		runner.fun = fun;
@@ -62,8 +62,8 @@ struct ThreadRunner {
 }
 
 extern(C) typeof(THREAD_RETURN_VALUE) runThread(bool AllowStopTheWorld)(ThreadRunner* runner) {
-    import d.gc.capi : __sd_gc_free;
-    import d.gc.thread : createThread, destroyThread;
+	import d.gc.capi : __sd_gc_free;
+	import d.gc.thread : createThread, destroyThread;
 
 	auto fun = runner.fun;
 	auto arg = runner.arg;
@@ -76,5 +76,5 @@ extern(C) typeof(THREAD_RETURN_VALUE) runThread(bool AllowStopTheWorld)(ThreadRu
 
 	fun(arg);
 
-    return THREAD_RETURN_VALUE;
+	return THREAD_RETURN_VALUE;
 }
