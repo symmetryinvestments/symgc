c2de7ecfeb37a2d4887f106a55347f26185d60d6
Steven Schveighoffer <schveiguy@gmail.com>
2025-07-10 18:22:33 -0400

    Refactor the flushing code in the thread bins

--- source/d/gc/tbin.d
+++ source/d/gc/tbin.d
@@ -160,21 +160,10 @@ public:
 	}
 
 	bool flush(ref CachedExtentMap emap, uint nretain) {
-		if (nretain >= ncached) {
+		auto size = ncached;
+		if (nretain >= size) {
 			return false;
 		}
-
-		// We precompute all we need so we can work out of local variables.
-		auto newHead = top;
-		auto oldHead = _head;
-		auto base = oldHead + nretain;
-
-		auto nlw = nlowWater;
-		auto nflush = ncached - nretain;
-		assert(nflush <= ncached,
-		       "Cannot flush more elements than are cached!");
-
-		auto worklist = base[0 .. nflush];
 
 		/**
 		 * TODO: Statistically, a lot of the pointers are going to
@@ -182,33 +171,29 @@ public:
 		 *       optimizations that can be done based on that fact.
 		 *       We need a batch emap lookup facility.
 		 */
+		auto nflush = size - nretain;
 		import core.stdc.stdlib : alloca;
 		auto pds = cast(PageDescriptor*) alloca(nflush * PageDescriptor.sizeof);
 
+		auto base = _head;
+		auto worklist = base[nretain .. size];
 		foreach (i, ptr; worklist) {
 			import d.gc.util;
 			auto aptr = alignDown(ptr, PageSize);
 			pds[i] = emap.lookup(aptr);
 		}
 
-		// Actually do flush to the arenas.
-		while (worklist.length > 0) {
-			auto ndeferred = pds[0].arena.batchFree(emap, worklist, pds);
-			worklist = base[0 .. ndeferred];
+		flushImpl(emap, worklist, pds);
+
+		// Adjust bin markers to reflect the flush.
+		_head = top - nretain;
+		if (nretain < nlowWater) {
+			_low_water = current;
 		}
 
 		// Move the remaining items on top of the stack if necessary.
-		if (nretain > 0) {
-			auto src = base;
-			while (src > oldHead) {
-				*(--newHead) = *(--src);
-			}
-		}
-
-		// Adjust bin markers to reflect the flush.
-		_head = newHead;
-		if (nretain < nlw) {
-			_low_water = current;
+		for (uint i = nretain; i-- > 0;) {
+			_head[i] = base[i];
 		}
 
 		/**
@@ -219,7 +204,7 @@ public:
 		 *        due to leftover pointers in there.
 		 */
 		auto ptr = bottom;
-		while (ptr < newHead) {
+		while (ptr < _head) {
 			*(ptr++) = null;
 		}
 
@@ -331,6 +316,15 @@ private:
 		_head++;
 		_low_water += PointerSize;
 		return true;
+	}
+
+	void flushImpl(ref CachedExtentMap emap, inout(void)*[] worklist,
+	               PageDescriptor* pds) {
+		// Actually do flush to the arenas.
+		while (worklist.length > 0) {
+			auto ndeferred = pds[0].arena.batchFree(emap, worklist, pds);
+			worklist = worklist[0 .. ndeferred];
+		}
 	}
 
 	void checkIsEarlier(ushort earlier, ushort later) const {
